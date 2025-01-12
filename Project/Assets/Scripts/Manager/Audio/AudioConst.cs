dddc06008dce5f610da2c081601f4e1988d6f03e
JiShouyuan <shouyuan.ji@dragonplus.com>
2025-01-12 18:36:28 +0800

    音效

--- Project/Assets/Scripts/Manager/Audio/AudioConst.cs
+++ Project/Assets/Scripts/Manager/Audio/AudioConst.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public partial class AudioConst
 {
-    public static readonly string sfx_pop_open = "";
-    public static readonly string panel_in = "";
-    public static readonly string button_s = "";
+    public static readonly string sfx_pop_open = "sfx_ui_inferface_open_close"; //弹窗弹出通用声音
+    public static readonly string panel_in = "sfx_ui_inferface_open_close"; //// 弹出框通用打开
+    public static readonly string button_s = "sfx_button_m"; // 通用按钮 
 }
