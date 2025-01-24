b97063ce8a54c1ea1d049bf58b1ad3a562971907
JiShouyuan <shouyuan.ji@dragonplus.com>
2025-01-24 15:32:03 +0800

    GF

---
+++ GF/Assets/GameMain/Runtime/Definition/Constant/Constant.Setting.cs
@@ -0,0 +1,28 @@
+﻿namespace GameMain
+{
+    public static partial class Constant
+    {
+        public static class Setting
+        {
+            public const string Language = "Setting.Language";
+            public const string SoundGroupMuted = "Setting.{0}Muted";
+            public const string SoundGroupVolume = "Setting.{0}Volume";
+            public const string MusicMuted = "Setting.MusicMuted";
+            public const string MusicVolume = "Setting.MusicVolume";
+            public const string SoundMuted = "Setting.SoundMuted";
+            public const string SoundVolume = "Setting.SoundVolume";
+            public const string UISoundMuted = "Setting.UISoundMuted";
+            public const string UISoundVolume = "Setting.UISoundVolume";
+
+            /// <summary>
+            /// 退出App时间
+            /// </summary>
+            public static readonly string QuitAppTime = "Setting.QuitAppTime";
+
+            /// <summary>
+            /// A/B测试组
+            /// </summary>
+            public static readonly string ABTestGroup = "Setting.ABTestGroup";
+        }
+    }
+}
